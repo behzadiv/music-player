b218d734571a9895dc2875330a845e77b7e1a8b2
Behzad <saeedibehzad@gmail.com>
2021-09-24 22:27:27 +0330

    logic of playBtn and music-range

--- script.js
+++ script.js
@@ -23,3 +23,33 @@ let musicCover = musics[currentMusic].cover
 let audio = musics[currentMusic].audio
 document.querySelector("#music-title").innerHTML = musicName
 document.querySelector("#music-cover").src = musicCover
+
+let range = document.querySelector("#range")
+let playBtn = document.querySelector("#play")
+
+audio.addEventListener("canplay",()=>{
+    range.max = audio.duration
+})
+audio.addEventListener("timeupdate",()=>{
+    range.value = audio.currentTime
+    if( range.value == Math.floor(range.max)){
+        document.getElementById("play").classList.replace("fa-pause","fa-play")
+         
+    }
+})
+range.addEventListener("input",()=>{
+    audio.currentTime = range.value
+})
+playBtn.addEventListener("click",()=>{
+    if(audio.paused){
+        audio.play()
+        document.getElementById("play").classList.replace("fa-play","fa-pause")
+        range.style
+        
+    }else{
+        audio.pause()
+        document.getElementById("play").classList.replace("fa-pause","fa-play")
+        
+    }
+    
+})
